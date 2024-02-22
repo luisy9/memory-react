9936d33590dc9f80827934698eb31450c821df05
Luis de Haro <luisdeharo4@gmail.com>
2024-02-22 13:17:14 +0100

    Memory, comprobaciones de si son pareja y que deje marcar mas de dos si hay pareja

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,12 +1,16 @@
-export const Items = ({ logos, setCardVisible }) => {
+export const Items = ({ logos, setCardVisible, disableButton }) => {
+    // console.log(logos)
+
+    const disabled = { pointerEvents: disableButton === 'disable' ? 'none' : 'auto' }
     return (
         <>
             {
                 logos.map(logo => {
                     return (
-                        <div className="border-2 border-white" key={logo.index}
+                        <div className="border-2 border-white" key={logo.index} style={disabled}
                             onClick={() => setCardVisible(logo.index, logo.isVisible)}>
-                            {logo.isVisible === false ? <div className="bg-[#7A8AA4] w-40 h-40 cursor-pointer"></div>
+                                <p>{disabled.pointerEvents}</p>
+                            {logo.isVisible === false ? <div className="bg-[#7A8AA4] w-40 h-40 cursor-pointer">{logo.img}</div>
                                 :
                                 <img src={logo.img} alt={logo.index} className="w-40 h-40" />}</div>
                     )
--- src/components/Memory.jsx
+++ src/components/Memory.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Items from "./Items";
 
 export const Memory = () => {
@@ -18,62 +18,82 @@ export const Memory = () => {
     { img: 'oppenhaimmer.webp', isVisible: false, index: 11 },
   ]);
 
+  const [disableButton, setDisableButton] = useState('');
   const [anterior1, setAnterior1] = useState({ index: '' });
   const [anterior2, setAnterior2] = useState({ index: '' });
+  const [parejaEncontrada, setParejaEncontrada] = useState(false);
 
 
-  function setCardVisible(index, isVisible) {
-    setLogos(logos.map(logo => {
-      if (logo.index === index) {
-        return { img: logo.img, isVisible: !logo.isVisible, index: logo.index }
+  useEffect(() => {
+    if ((anterior1.index !== '') && (anterior2.index !== '')) {
+      compareImagenes();
+    }
+  }, [anterior1, anterior2])
+
+
+  function setCardVisible(index) {
+    //Logica para que solo se puedan marcar dos cards
+
+    setLogos(logos => {
+      const sonTrue = logos.filter(e => e.isVisible === true);
+      if (parejaEncontrada === false) {
+        if (sonTrue.length < 2) {
+          return logos.map(logo => logo.index === index ? { ...logo, isVisible: true } : logo);
+        } else {
+          return [...logos];
+        }
       } else {
-        return logo
+        console.log('entra')
+        if (sonTrue.length < (sonTrue.length + sonTrue.length)) {
+          return logos.map(logo => logo.index === index ? { ...logo, isVisible: true } : logo);
+        } else {
+          return [...logos];
+        }
       }
-    }));
+    });
 
     //Setear Imagenes para la comparación
-    let imagen1;
-    let imagen2;
-
-    if (anterior1.index === '') {
-      imagen1 = { index: index };
-      setAnterior1({ index: index });
-    } else if (anterior1.index !== '') {
-      imagen2 = { index: index }
-      setAnterior2({ index: index });
-    }
-
-    if (imagen1 && imagen2) {
-      compareImagenes();
+    if ((anterior1.index !== '') && (anterior2.index !== '')) {
+      // setDisableButton('disable');
+    } else {
+      // setDisableButton('')
+      if (anterior1.index === '') {
+        setAnterior1({ index: index });
+      } else if (anterior1.index !== '') {
+        setAnterior2({ index: index });
+      }
     }
   }
 
   function compareImagenes() {
-    const logos = [
-      { img: 'cocacola.webp', isVisible: false, index: 0 },
-      { img: 'davidgoggins.webp', isVisible: false, index: 1 },
-      { img: 'milka.svg', isVisible: false, index: 2 },
-      { img: 'nestea.webp', isVisible: false, index: 3 },
-      { img: 'cocacola.webp', isVisible: false, index: 4 },
-      { img: 'davidgoggins.webp', isVisible: false, index: 5 },
-      { img: 'nestea.webp', isVisible: false, index: 6 },
-      { img: 'elonmusk.webp', isVisible: false, index: 7 },
-      { img: 'elonmusk.webp', isVisible: false, index: 8 },
-      { img: 'oppenhaimmer.webp', isVisible: false, index: 9 },
-      { img: 'milka.svg', isVisible: false, index: 10 },
-      { img: 'oppenhaimmer.webp', isVisible: false, index: 11 },
-    ];
-
     /* Ahora lo que tenemos que hacer es lo siguiente: tenemos que poner en Visible si coinciden el valorAnterior1 y el valorAnterior2 
     (si tienen el mismo nombre de imagen), 
     si no coinciden pues que haya un setTimeout y que las dos imagenes vuelvan a estar en isVisible: false  */
-
+    setLogos(logos => {
+      if (logos[anterior1.index].img === logos[anterior2.index].img) {
+        setDisableButton('');
+        setAnterior1({ index: '' });
+        setAnterior2({ index: '' });
+        setParejaEncontrada(true);
+        return [...logos];
+      } else {
+        setTimeout(() => {
+          setLogos(logos.map(logo => {
+            if (logos[anterior1.index].img !== logos[anterior2.index].img) {
+              setDisableButton('');
+              setAnterior1({ index: '' });
+              setAnterior2({ index: '' });
+              return { ...logo, isVisible: false };
+            }
+          }));
+        }, 2000);
+        return logos;
+      }
+    });
   }
 
 
-  function shuffle() {
-
-  }
+  function shuffle() { }
 
 
   return (
@@ -82,7 +102,7 @@ export const Memory = () => {
       <p className="text-center text-6xl">{anterior2.index}</p>
       <div className="flex justify-center items-center h-screen">
         <div className="grid grid-cols-4">
-          <Items logos={logos} setCardVisible={setCardVisible} />
+          <Items logos={logos} setCardVisible={setCardVisible} disableButton={disableButton} />
         </div>
       </div>
       <div className="flex justify-center">
