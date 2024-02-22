965c1fa915e82a90c0c71127a6ebd1cda492f056
Luis de Haro <luisdeharo4@gmail.com>
2024-02-22 13:37:01 +0100

    he quitado el texto

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -9,8 +9,8 @@ export const Items = ({ logos, setCardVisible, disableButton }) => {
                     return (
                         <div className="border-2 border-white" key={logo.index} style={disabled}
                             onClick={() => setCardVisible(logo.index, logo.isVisible)}>
-                                <p>{disabled.pointerEvents}</p>
-                            {logo.isVisible === false ? <div className="bg-[#7A8AA4] w-40 h-40 cursor-pointer">{logo.img}</div>
+
+                            {logo.isVisible === false ? <div className="bg-[#7A8AA4] w-40 h-40 cursor-pointer"></div>
                                 :
                                 <img src={logo.img} alt={logo.index} className="w-40 h-40" />}</div>
                     )
